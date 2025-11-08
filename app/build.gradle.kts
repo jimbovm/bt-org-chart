eab48d1acbc61bde7623164ea221598a116fdf32
James Brierley <jmb8710@gmail.com>
2025-11-08 08:08:50 +0000

    display proper test output

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,7 @@ application {
 tasks.named<Test>("test") {
     // Use JUnit Platform for unit tests.
     useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed", "standardOut", "standardError")
+    }
 }
