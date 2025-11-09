5db9561fc75af311064b4978df1dd7929de26a11
James Brierley <jmb8710@gmail.com>
2025-11-09 05:53:26 +0000

    rm old class, add parser implementation

--- app/src/main/java/com/github/jimbovm/bt/orgchart/parser/Parser.java
+++ app/src/main/java/com/github/jimbovm/bt/orgchart/parser/Parser.java
@@ -1,9 +1,19 @@
 package com.github.jimbovm.bt.orgchart.parser;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Objects;
 import java.util.logging.Logger;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
-import com.github.jimbovm.bt.orgchart.EmployeeRecord;
+import com.github.jimbovm.bt.orgchart.Employee;
 
 /**
  * Parser for org chart text files.
@@ -25,17 +35,46 @@ public final class Parser {
 	}
 
 	/**
+	 * Return a stream from an InputStream for lazy line-by-line reading.
+	 * 
+	 * @param inputStream The InputStream from which to read.
+	 * @return A Stream<String> to a reader over the passed InputStream.
+	 * @throws IOException if there is a problem accessing the InputStream.
+	 */
+	private static Stream<String> getStream(InputStream inputStream) throws IOException {
+
+		Objects.requireNonNull(inputStream);
+		var reader = new BufferedReader(new InputStreamReader(inputStream));
+		return reader.lines();
+	}
+
+	/**
+	 * Return a stream from an InputStream for lazy line-by-line reading.
+	 * 
+	 * @param filePath The path to a file from which to read.
+	 * @return A Stream<String> to a reader over the file passed.
+	 * @throws IOException if there is a problem accessing the file.
+	 */
+	private static Stream<String> getFileAsStream(String filePath) throws IOException {
+
+		Objects.requireNonNull(filePath);
+		final var path = Path.of(filePath);
+		logger.info(String.format("Opening org chart file %s", filePath));
+		return Files.lines(path, StandardCharsets.UTF_8);
+	}
+
+	/**
 	 * Parse a single employee record.
 	 * 
 	 * If the manager field is blank, it will be set to the employee's own ID (i.e.
-	 * they are their "own manager").
+	 * they are the head of the company and thus their "own manager").
 	 * 
 	 * @param line The line to parse.
 	 * @return An instance of EmployeeRecord representing the parsed record.
 	 * @throws IllegalArgumentException if the line cannot be parsed according to
 	 *                                  the format.
 	 */
-	public static EmployeeRecord parseLine(String line) throws IllegalArgumentException {
+	public static Employee parseLine(String line) throws IllegalArgumentException {
 
 		var matcher = ENTRY_PATTERN.matcher(line);
 		matcher.find();
@@ -60,7 +99,53 @@ public final class Parser {
 			throw new IllegalArgumentException("Malformed line", e);
 		}
 
-		logger.info(String.format("From line \"%s\" read id: %d, name: %s, manager: %d", line, id, name, manager));
-		return new EmployeeRecord(id, name, manager);
+		logger.info(String.format("From line \"%s\" read id: %d, name: %s, manager: %d", line, id, name,
+				manager));
+		return new Employee(id, name, manager);
+	}
+
+	/**
+	 * Parse all lines from an instance of Stream<String>.
+	 * 
+	 * @param stream The stream from which to parse.
+	 * @return A List of EmployeeRecord instances.
+	 */
+	private static List<Employee> parseLines(Stream<String> stream) {
+
+		List<Employee> records = stream
+				.skip(1) // ignore the header
+				.map(String::trim) // strip whitespace (defensive)
+				.filter((line) -> !(line.isEmpty() || line.isBlank())) // ignore blank lines
+				.map(Parser::parseLine) // lazy line parse
+				.toList(); // finalize
+
+		logger.info(String.format("Parsed %d records", records.size()));
+		return records;
+	}
+
+	/**
+	 * Parse an employee file from an InputStream.
+	 * 
+	 * @param inputStream The stream from which to parse.
+	 * @return A List of EmployeeRecord instances.
+	 * @throws IOException if there is a problem accessing the stream.
+	 */
+	public static List<Employee> parse(InputStream inputStream) throws IOException {
+
+		var stream = getStream(inputStream);
+		return parseLines(stream);
+	}
+
+	/**
+	 * Parse an employee file from a file on the filesystem.
+	 * 
+	 * @param filePath The path to the file from which to parse.
+	 * @return A List of EmployeeRecord instances.
+	 * @throws IOException if there is a problem accessing the file.
+	 */
+	public static List<Employee> parse(String filePath) throws IOException {
+
+		var stream = getFileAsStream(filePath);
+		return parseLines(stream);
 	}
 }
