251417084b852a9a1d27bfc5a206171a15bba58f
James Brierley <jmb8710@gmail.com>
2025-11-09 09:21:04 +0000

    add inspection methods

--- app/src/main/java/com/github/jimbovm/bt/orgchart/Employee.java
+++ app/src/main/java/com/github/jimbovm/bt/orgchart/Employee.java
@@ -1,5 +1,7 @@
 package com.github.jimbovm.bt.orgchart;
 
+import com.google.common.base.Optional;
+
 /**
  * Represents an employee record as parsed from an org chart text file.
  */
@@ -7,4 +9,16 @@ public record Employee(
 		int id,
 		String name,
 		int manager) {
+
+	public boolean isChief() {
+		return this.id == this.manager;
+	}
+
+	public boolean manages(Employee employee) {
+		return (employee == null) || (this.manager == employee.id());
+	}
+
+	public boolean manages(Optional<Employee> employee) {
+		return (employee.orNull() == null) || (this.manager() == employee.get().id());
+	}
 }
