e747d4daf22ff947813833078d7a787b665c635f
James Brierley <jmb8710@gmail.com>
2025-11-12 19:53:39 +0000

    add documentation and correct import

--- app/src/main/java/com/github/jimbovm/bt/orgchart/Employee.java
+++ app/src/main/java/com/github/jimbovm/bt/orgchart/Employee.java
@@ -1,6 +1,6 @@
 package com.github.jimbovm.bt.orgchart;
 
-import com.google.common.base.Optional;
+import java.util.Optional;
 
 /**
  * Represents an employee record as parsed from an org chart text file.
@@ -10,18 +10,42 @@ public record Employee(
 		String name,
 		int manager) {
 
+	/**
+	 * Check if this employee is the chief of the organisation, that is, they are
+	 * their "own manager".
+	 * 
+	 * @return True if this employee's ID equals their manager's, false otherwise.
+	 */
 	public boolean isChief() {
 		return this.id == this.manager;
 	}
 
+	/**
+	 * Check if an employee is the manager of this one.
+	 * 
+	 * @param employee An Employee instance, who may be this employee's manager.
+	 * @return True if this employee manages the employee given as a parameter.
+	 */
 	public boolean manages(Employee employee) {
 		return (employee == null) || (this.manager == employee.id());
 	}
 
+	/**
+	 * Check if an employee is the manager of this one.
+	 * 
+	 * @param employee An Optional wrapping an Employee instance, who may be this
+	 *                 employee's manager.
+	 * @return True if this employee manages the employee given as a parameter.
+	 */
 	public boolean manages(Optional<Employee> employee) {
-		return (employee.orNull() == null) || (this.manager() == employee.get().id());
+		return (employee.isEmpty()) || (this.manager() == employee.get().id());
 	}
 
+	/**
+	 * Return a string representation of an Employee instance.
+	 * 
+	 * @return A string representation of the form "[employee name] (employee ID)".
+	 */
 	public String toString() {
 		return String.format("%s (%d)", this.name, this.id);
 	}
